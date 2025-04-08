096fb42dd5e2d6bbe4ed24d7bce560a52627e711
Daniel Osorio <danioshi@gmail.com>
2025-04-08 13:18:53 +0200

    add helmet middleware for improved security

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import helmet from 'helmet'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -9,6 +10,7 @@ import logRequest from '@src/middleware/logRequest'
 const app = express()
 const port = 80
 
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
