78b08c84784d6c0f2add36571ce9dede1df83fe3
Daniel Osorio <danioshi@gmail.com>
2025-04-16 10:44:42 +0200

    refactor getTask to include next function for improved error handling

--- src/api/v1.0/task/getTask.ts
+++ src/api/v1.0/task/getTask.ts
@@ -1,12 +1,13 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import Task from '@src/models/Task'
 
-const getTask = async (req: Request, res: Response) => {
+const getTask = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const taskId = req.params.taskId
     const task = await Task.findById(taskId)
     if (!task) {
       res.status(404).json({ error: 'Task not found' })
+      return next(new Error())
     }
     res.status(200).json(task)
   } catch (error) {
