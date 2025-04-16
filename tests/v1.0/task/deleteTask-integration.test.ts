e9ae10e9bf182f1d11a15b6a3a6df527b1c8631a
Daniel Osorio <danioshi@gmail.com>
2025-04-16 13:37:02 +0200

    refactor deleteTask integration tests for improved organization and clarity

--- tests/v1.0/task/deleteTask-integration.test.ts
+++ tests/v1.0/task/deleteTask-integration.test.ts
@@ -29,40 +29,43 @@ describe('Delete Task', () => {
     await disconnectInMemoryDB()
   })
 
-  it('should delete a task', async () => {
+  describe('Successful Cases', () => {
+    it('should delete a task', async () => {
+      const mockedTask = {
+        title: 'Test task',
+        description: 'Test task description',
+        status: 'pending',
+        dueDate: '2036-03-23T00:00:00.000Z',
+        creationDate: new Date('2025-04-08T12:23:16.476Z'),
+      }
+      const createdTask = await Task.create(mockedTask)
+      const taskId = createdTask._id.toString()
 
-    const mockedTask = {
-      title: 'Test task',
-      description: 'Test task description',
-      status: 'pending',
-      dueDate: '2036-03-23T00:00:00.000Z',
-      creationDate: new Date('2025-04-08T12:23:16.476Z'),
-    }
-    const createdTask = await Task.create(mockedTask)
-    const taskId = createdTask._id.toString()
-
-    const response = await supertest(app).delete(path.replace(':taskId', taskId))
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual({ message: 'Task deleted successfully' })
+      const response = await supertest(app).delete(path.replace(':taskId', taskId))
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual({ message: 'Task deleted successfully' })
+    })
   })
 
-  it('should return a 404 error if the task does not exist', async () => {
-    const nonExistentTaskId = '67ff95ce036a26e20e4b3303'
-    const response = await supertest(app).delete(path.replace(':taskId', nonExistentTaskId))
-    expect(response.status).toBe(404)
-    expect(response.body).toEqual({ error: 'Task not found' })
-  })
+  describe('Failure Cases', () => {
+    it('should return a 404 error if the task does not exist', async () => {
+      const nonExistentTaskId = '67ff95ce036a26e20e4b3303'
+      const response = await supertest(app).delete(path.replace(':taskId', nonExistentTaskId))
+      expect(response.status).toBe(404)
+      expect(response.body).toEqual({ error: 'Task not found' })
+    })
 
-  it('should return a 400 error for invalid taskId format', async () => {
-    const response = await supertest(app).delete(path.replace(':taskId', 'invalidTaskId'))
-    expect(response.status).toBe(400)
-    expectExpressValidatorError(response, 'taskId', 'params')
-  })
+    it('should return a 400 error for invalid taskId format', async () => {
+      const response = await supertest(app).delete(path.replace(':taskId', 'invalidTaskId'))
+      expect(response.status).toBe(400)
+      expectExpressValidatorError(response, 'taskId', 'params')
+    })
 
-  it('should return a 500 error if there is a database error', async () => {
-    taskFindByIdAndDeleteSpy.mockRejectedValueOnce(new Error('Server error'))
-    const response = await supertest(app).delete(path.replace(':taskId', '67ff95ce036a26e20e4b3303'))
-    expect(response.status).toBe(500)
-    expect(response.body).toEqual({ error: 'Internal Server Error' })
+    it('should return a 500 error if there is a database error', async () => {
+      taskFindByIdAndDeleteSpy.mockRejectedValueOnce(new Error('Server error'))
+      const response = await supertest(app).delete(path.replace(':taskId', '67ff95ce036a26e20e4b3303'))
+      expect(response.status).toBe(500)
+      expect(response.body).toEqual({ error: 'Internal Server Error' })
+    })
   })
 })
