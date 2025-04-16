73798ded6b6947ee356da38ef67654fe24283c62
Daniel Osorio <danioshi@gmail.com>
2025-04-16 10:40:43 +0200

    refactor updateTask to include error handling and next function for improved middleware support

--- src/api/v1.0/task/updateTask.ts
+++ src/api/v1.0/task/updateTask.ts
@@ -1,17 +1,19 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import Task from '@src/models/Task'
 
-const updateTask = async (req: Request, res: Response) => {
+const updateTask = async (req: Request, res: Response, next: NextFunction) => {
   const taskId = req.params.taskId
   const taskUpdates = req.body
   if (!taskUpdates || Object.keys(taskUpdates).length === 0) {
     res.status(400).json({ error: 'No updates provided' })
+    throw new Error('No updates provided')
   }
 
   try {
     const task = await Task.findByIdAndUpdate(taskId, taskUpdates, { new: true })
     if (!task) {
       res.status(404).json({ error: 'Task not found' })
+      return next(new Error())
     }
     res.status(200).json({ message: 'Task updated successfully' })
   } catch (error) {
