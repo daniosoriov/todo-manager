724f4dc9b631cd63d79b82d15b037d251397c158
Daniel Osorio <danioshi@gmail.com>
2025-04-16 13:36:11 +0200

    refactor getTask integration tests for improved structure and clarity

--- tests/v1.0/task/getTask-integration.test.ts
+++ tests/v1.0/task/getTask-integration.test.ts
@@ -14,7 +14,7 @@ app.use(express.json())
 const path = '/v1.0/task/:taskId'
 app.get(path, getTaskValidators, fieldValidation, getTask)
 
-describe('Successful Cases', () => {
+describe('Get Task', () => {
   beforeAll(async () => {
     await connectInMemoryDB()
   })
@@ -23,70 +23,64 @@ describe('Successful Cases', () => {
     await disconnectInMemoryDB()
   })
 
-  it('should get a task successfully', async () => {
-    // Insert a task into the in-memory database
-    const mockedTask = await Task.create({
-      title: 'Test task',
-      description: 'Test task description',
-      status: 'pending',
-      dueDate: '2036-03-23T00:00:00.000Z',
-      creationDate: new Date('2025-04-08T12:23:16.476Z'),
+  describe('Successful Cases', () => {
+    it('should get a task successfully', async () => {
+      // Insert a task into the in-memory database
+      const mockedTask = await Task.create({
+        title: 'Test task',
+        description: 'Test task description',
+        status: 'pending',
+        dueDate: '2036-03-23T00:00:00.000Z',
+        creationDate: new Date('2025-04-08T12:23:16.476Z'),
+      })
+
+      const taskId = mockedTask._id.toString()
+      const taskFindByIdSpy = vi.spyOn(Task, 'findById')
+      const response = await supertest(app).get(path.replace(':taskId', taskId))
+
+      expect(response.status).toBe(200)
+      expect(response.body).toEqual({
+        _id: taskId,
+        title: mockedTask.title,
+        description: mockedTask.description,
+        status: mockedTask.status,
+        dueDate: mockedTask.dueDate.toISOString(),
+        creationDate: mockedTask.creationDate.toISOString(),
+        __v: mockedTask.__v,
+      })
+      expect(taskFindByIdSpy).toHaveBeenCalledWith(taskId)
+    })
+  })
+
+  describe('Failure Cases', () => {
+    it('should return a 400 error for invalid taskId format', async () => {
+      const response = await supertest(app).get(path.replace(':taskId', 'invalidTaskId'))
+      expect(response.status).toBe(400)
+      expectExpressValidatorError(response, 'taskId', 'params')
     })
 
-    const taskId = mockedTask._id.toString()
-    const taskFindByIdSpy = vi.spyOn(Task, 'findById')
-    const response = await supertest(app).get(path.replace(':taskId', taskId))
-
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual({
-      _id: taskId,
-      title: mockedTask.title,
-      description: mockedTask.description,
-      status: mockedTask.status,
-      dueDate: mockedTask.dueDate.toISOString(),
-      creationDate: mockedTask.creationDate.toISOString(),
-      __v: mockedTask.__v,
-    })
-    expect(taskFindByIdSpy).toHaveBeenCalledWith(taskId)
-  })
-})
-
-describe('Failure Cases', () => {
-  beforeAll(async () => {
-    await connectInMemoryDB()
-  })
-
-  afterAll(async () => {
-    await disconnectInMemoryDB()
-  })
-
-  it('should return a 400 error for invalid taskId format', async () => {
-    const response = await supertest(app).get(path.replace(':taskId', 'invalidTaskId'))
-    expect(response.status).toBe(400)
-    expectExpressValidatorError(response, 'taskId', 'params')
-  })
-
-  it('should return a 404 error for non-existing taskId', async () => {
-    const nonExistingTaskId = '603d2f4e4f1a2c001f8b4567'
-    const response = await supertest(app).get(path.replace(':taskId', nonExistingTaskId))
-    expect(response.status).toBe(404)
-    expect(response.body).toEqual({ error: 'Task not found' })
-  })
-
-  it('should fail when no taskId is provided', async () => {
-    const response = await supertest(app).get(path.replace(':taskId', ''))
-    expect(response.status).toBe(404)
-    expect(response.body).toEqual({})
-  })
-
-  it('should return a 500 error for database error', async () => {
-    const taskId = '603d2f4e4f1a2c001f8b4567'
-    vi.spyOn(Task, 'findById').mockImplementationOnce(() => {
-      throw new Error('Database error')
+    it('should return a 404 error for non-existing taskId', async () => {
+      const nonExistingTaskId = '603d2f4e4f1a2c001f8b4567'
+      const response = await supertest(app).get(path.replace(':taskId', nonExistingTaskId))
+      expect(response.status).toBe(404)
+      expect(response.body).toEqual({ error: 'Task not found' })
     })
 
-    const response = await supertest(app).get(path.replace(':taskId', taskId))
-    expect(response.status).toBe(500)
-    expect(response.body).toEqual({ error: 'Internal Server Error' })
+    it('should fail when no taskId is provided', async () => {
+      const response = await supertest(app).get(path.replace(':taskId', ''))
+      expect(response.status).toBe(404)
+      expect(response.body).toEqual({})
+    })
+
+    it('should return a 500 error for database error', async () => {
+      const taskId = '603d2f4e4f1a2c001f8b4567'
+      vi.spyOn(Task, 'findById').mockImplementationOnce(() => {
+        throw new Error('Database error')
+      })
+
+      const response = await supertest(app).get(path.replace(':taskId', taskId))
+      expect(response.status).toBe(500)
+      expect(response.body).toEqual({ error: 'Internal Server Error' })
+    })
   })
 })
