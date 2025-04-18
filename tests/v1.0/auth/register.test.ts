acdb3d5796c7bfa856171203e47670fd27f3e958
Daniel Osorio <danioshi@gmail.com>
2025-04-18 14:50:02 +0200

    test: refactor user registration tests to use centralized request setup

--- tests/v1.0/auth/register.test.ts
+++ tests/v1.0/auth/register.test.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { reqAuth, res } from '@tests/utils/unitTestSetup'
 import register from '@src/api/v1.0/auth/register'
 import User from '@src/models/User'
 
@@ -9,18 +10,6 @@ vi.mock('@src/models/User', () => ({
   },
 }))
 
-const req = {
-  body: {
-    email: 'test@test.com',
-    password: 'password',
-  },
-} as Partial<Request>
-
-const res = {
-  status: vi.fn().mockReturnThis(),
-  json: vi.fn(),
-} as Partial<Response>
-
 console.error = vi.fn()
 
 describe('Register User Unit Tests', () => {
@@ -29,16 +18,16 @@ describe('Register User Unit Tests', () => {
   })
 
   it('should register a user successfully', async () => {
-    vi.mocked(User.create).mockResolvedValueOnce(req.body)
-    await register(req as Request, res as Response)
+    vi.mocked(User.create).mockResolvedValueOnce(reqAuth.body)
+    await register(reqAuth as Request, res as Response)
     expect(res.status).toHaveBeenCalledWith(201)
     expect(res.json).toHaveBeenCalledWith({ message: 'User registered successfully' })
-    expect(User.create).toHaveBeenCalledWith(req.body)
+    expect(User.create).toHaveBeenCalledWith(reqAuth.body)
   })
 
   it('should not register a user when there is a database error', async () => {
     vi.mocked(User.create).mockRejectedValueOnce(new Error('Database error'))
-    await register(req as Request, res as Response)
+    await register(reqAuth as Request, res as Response)
     expect(res.status).toHaveBeenCalledWith(500)
     expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' })
   })
