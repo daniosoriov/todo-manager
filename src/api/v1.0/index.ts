4accf2a8d983ee20d639f0efd6ab6fc06036e2bf
Daniel Osorio <danioshi@gmail.com>
2025-04-18 09:42:47 +0200

    feat: integrate authentication router into API version 1

--- src/api/v1.0/index.ts
+++ src/api/v1.0/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import authRouter from '@src/api/v1.0/auth/authRouter'
 import taskRouter from '@src/api/v1.0/task/taskRouter'
 
 const apiRouterV1 = express.Router()
 
+apiRouterV1.use('/auth', authRouter)
 apiRouterV1.use('/task', taskRouter)
 
 export default apiRouterV1
