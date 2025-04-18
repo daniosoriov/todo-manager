874833bfc4ac5db812e949b821a11706982d32c8
Daniel Osorio <danioshi@gmail.com>
2025-04-18 15:04:10 +0200

    feat: add routes for user registration and login in authRouter

--- src/api/v1.0/auth/authRouter.ts
+++ src/api/v1.0/auth/authRouter.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import login from '@src/api/v1.0/auth/login'
+import register from '@src/api/v1.0/auth/register'
 
 const authRouter = express.Router()
 
+authRouter.post('/register', register)
+authRouter.post('/login', login)
 
 export default authRouter
