d57d985e5ab8db146886ef274cf58f83ef2e8860
Daniel Osorio <danioshi@gmail.com>
2025-04-22 15:32:46 +0200

    feat: add userId field to task schema and enable timestamps

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -2,28 +2,32 @@ import { Schema, model } from 'mongoose'
 import { taskStatus } from '@src/types/types'
 
 const taskSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  status: {
-    type: String,
-    enum: taskStatus,
-    default: 'pending',
-  },
-  creationDate: {
-    type: Date,
-    default: Date.now,
-  },
-  dueDate: {
-    type: Date,
-    required: true,
-  },
-})
+      title: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: false,
+      },
+      status: {
+        type: String,
+        enum: taskStatus,
+        default: 'pending',
+      },
+      dueDate: {
+        type: Date,
+        required: true,
+      },
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+    })
 
 const Task = model('Task', taskSchema)
 
