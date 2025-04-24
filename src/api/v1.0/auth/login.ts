e9f368ab0a41e6dcbe8d7c8068a6dce099e57e88
Daniel Osorio <danioshi@gmail.com>
2025-04-24 21:07:04 +0200

    feat: refactor login to use centralized token generation and storage

--- src/api/v1.0/auth/login.ts
+++ src/api/v1.0/auth/login.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
+import generateAndStoreTokens from '@src/utils/generateAndStoreTokens'
 import User from '@src/models/User'
-import Token from '@src/models/Token'
 
 const login = async (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -15,10 +15,8 @@ const login = async (req: Request, res: Response) => {
       res.status(401).json({ message: 'Invalid credentials' })
       return
     }
-    const token = await user.generateAuthToken()
-    const refreshToken = await user.generateRefreshToken()
-    Token.findOneAndReplace({ userId: user._id }, { token: refreshToken }, { upsert: true })
-    res.status(200).json({ token, refreshToken })
+    const { newToken, newRefreshToken } = await generateAndStoreTokens(user)
+    res.status(200).json({ token: newToken, refreshToken: newRefreshToken })
   } catch (error) {
     console.error('Error logging in:', error)
     res.status(500).json({ error: 'Internal Server Error' })
